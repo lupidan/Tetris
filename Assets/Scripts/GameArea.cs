5008098e57898d6cca17a2719baab5f8ac13597a
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 14:24:10 +0300

    Adds GameArea script

---
+++ Assets/Scripts/GameArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameArea : MonoBehaviour
+{
+	public Rect PlayArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+
+	public void AdjustTetrominoPosition(Tetromino tetromino)
+	{
+		LimitTetrominoInPlayArea(tetromino);
+	}
+
+	private void LimitTetrominoInPlayArea(Tetromino tetromino)
+	{
+		if (tetromino == null)
+			return;
+
+		for(int i=0; i < tetromino.ChildBlocks.Length; ++i)
+		{
+			while (tetromino.ChildBlocks[i].position.x < PlayArea.xMin)
+				tetromino.MoveRight();
+
+			while (tetromino.ChildBlocks[i].position.x > PlayArea.xMax)
+				tetromino.MoveLeft();
+
+			while (tetromino.ChildBlocks[i].position.y < PlayArea.yMin)
+				tetromino.MoveUp();
+
+			while (tetromino.ChildBlocks[i].position.y > PlayArea.yMax)
+				tetromino.MoveDown();
+		}
+	}
+}
