5d1975ec28e4f476ec6d390098503e4223b421a5
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 14:25:50 +0300

    Editor code to highlight area

---
+++ Assets/Scripts/Editor/GameAreaEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameArea))]
+public class PlayerShipEditor : Editor
+{
+
+    [DrawGizmo(GizmoType.Selected)]
+    static void DrawGameArea(GameArea gameArea, GizmoType gizmoType)
+    {
+        Gizmos.color = Color.red;
+		Rect rect = gameArea.PlayArea;
+        Gizmos.DrawLine(new Vector3(rect.xMin, rect.yMin, 0.0f), new Vector3(rect.xMax, rect.yMin, 0.0f));
+        Gizmos.DrawLine(new Vector3(rect.xMax, rect.yMin, 0.0f), new Vector3(rect.xMax, rect.yMax, 0.0f));
+        Gizmos.DrawLine(new Vector3(rect.xMax, rect.yMax, 0.0f), new Vector3(rect.xMin, rect.yMax, 0.0f));
+        Gizmos.DrawLine(new Vector3(rect.xMin, rect.yMax, 0.0f), new Vector3(rect.xMin, rect.yMin, 0.0f));
+    }
+
+}
