ad060d3d910c14ca934df3b0f91cd0e3f055b5f2
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 14:31:42 +0300

    Adds TetrominoController class

---
+++ Assets/Scripts/TetrominoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrominoController : MonoBehaviour
+{
+	public Tetromino ActiveTetromino;
+	public GameArea GameArea;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			this.ActiveTetromino.MoveRight();
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			this.ActiveTetromino.MoveLeft();
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			this.ActiveTetromino.HardDrop();
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			this.ActiveTetromino.MoveDown();
+		if (Input.GetKeyDown(KeyCode.Z))
+			this.ActiveTetromino.RotateClockwise();
+		if (Input.GetKeyDown(KeyCode.X))
+			this.ActiveTetromino.RotateCounterClockwise();
+
+		this.GameArea.AdjustTetrominoPosition(this.ActiveTetromino);
+	}
+
+}
