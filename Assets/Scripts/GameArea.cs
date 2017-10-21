30d0df03d32ac1c6d6c65609333dc867390678f2
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 14:43:06 +0300

    Play area is readonly now. Settable in the editor.

--- Assets/Scripts/GameArea.cs
+++ Assets/Scripts/GameArea.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameArea : MonoBehaviour
 {
-	public Rect PlayArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+	public Rect PlayArea { get { return _playArea; } }
+	
+	[SerializeField] private Rect _playArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
 
 	public void AdjustTetrominoPosition(Tetromino tetromino)
 	{
@@ -18,16 +20,16 @@ public class GameArea : MonoBehaviour
 
 		for(int i=0; i < tetromino.ChildBlocks.Length; ++i)
 		{
-			while (tetromino.ChildBlocks[i].position.x < PlayArea.xMin)
+			while (tetromino.ChildBlocks[i].position.x < this._playArea.xMin)
 				tetromino.MoveRight();
 
-			while (tetromino.ChildBlocks[i].position.x > PlayArea.xMax)
+			while (tetromino.ChildBlocks[i].position.x > this._playArea.xMax)
 				tetromino.MoveLeft();
 
-			while (tetromino.ChildBlocks[i].position.y < PlayArea.yMin)
+			while (tetromino.ChildBlocks[i].position.y < this._playArea.yMin)
 				tetromino.MoveUp();
 
-			while (tetromino.ChildBlocks[i].position.y > PlayArea.yMax)
+			while (tetromino.ChildBlocks[i].position.y > this._playArea.yMax)
 				tetromino.MoveDown();
 		}
 	}
