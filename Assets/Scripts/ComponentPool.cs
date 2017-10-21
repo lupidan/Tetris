71dffe839e89e2f119b4093f852cb0b9c1dfbb05
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 22:20:25 +0300

    Adds component pool

---
+++ Assets/Scripts/ComponentPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentPool<T> where T : MonoBehaviour
+{
+    private Func<T> _instantiateAction;
+    private Action<T> _getComponentAction;
+    private Action<T> _returnComponentAction;
+    private Stack<T> _pooledObjects;
+    private int lastInstantiatedAmount;
+
+    public ComponentPool(int initialPoolSize, Func<T> instantiateFunction, Action<T> getComponentAction = null, Action<T> returnComponentAction = null)
+    {
+        this._instantiateAction = instantiateFunction;
+        this._getComponentAction = getComponentAction;
+        this._returnComponentAction = returnComponentAction;
+        this._pooledObjects = new Stack<T>();
+        InstantiateComponentsIntoPool(initialPoolSize);
+    }
+
+    public T Get()
+    {
+        if (this._pooledObjects.Count == 0)
+            InstantiateComponentsIntoPool((lastInstantiatedAmount * 2) + 1);
+
+        T component = _pooledObjects.Pop();
+        if (_getComponentAction != null)
+            _getComponentAction(component);
+
+        return _pooledObjects.Pop();
+    }
+
+    public void Return(T component)
+    {
+        _pooledObjects.Push(component);
+        if (_returnComponentAction != null)
+            _returnComponentAction(component);
+    }
+
+    private void InstantiateComponentsIntoPool(int nComponents)
+    {
+        for (int i = 0; i < nComponents; i++)
+        {
+            var pooledObject = this._instantiateAction();
+            _pooledObjects.Push(pooledObject);
+        }
+        
+        lastInstantiatedAmount = _pooledObjects.Count;
+    }
+}
