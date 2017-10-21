7719f2157ae0b6d21598fa0ed5f783bd1c360b03
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-21 19:16:42 +0300

    Cambia a espacios

--- Assets/Scripts/TetrominoMovement.cs
+++ Assets/Scripts/TetrominoMovement.cs
@@ -2,16 +2,16 @@
 
 public struct TetrominoMovement
 {
-	public Tetromino Tetromino;
-	public Vector3 PreviousPosition;
-	public Vector3 PreviousRotation;
-	public bool MovementInvolvesCommit;
+    public Tetromino Tetromino;
+    public Vector3 PreviousPosition;
+    public Vector3 PreviousRotation;
+    public bool MovementInvolvesCommit;
 
-	public bool DidMove
-	{
-		get
-		{
-			return this.Tetromino.transform.position != PreviousPosition || this.Tetromino.transform.eulerAngles != PreviousRotation;
-		}
-	}
+    public bool DidMove
+    {
+        get
+        {
+            return this.Tetromino.transform.position != PreviousPosition || this.Tetromino.transform.eulerAngles != PreviousRotation;
+        }
+    }
 }
