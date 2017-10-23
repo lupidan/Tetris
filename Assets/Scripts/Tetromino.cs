2851f9a870d36dcb8bc33724b126d731c9367089
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-23 23:49:15 +0300

    Adds positioning offset for the tetrominos

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    public Vector3 PositioningOffset { get { return _positioningOffset; } }
     public Block[] ChildBlocks { get { return _childBlocks; } }
 
+    [SerializeField] private Vector3 _positioningOffset;
     [SerializeField] private Block[] _childBlocks;
 }
