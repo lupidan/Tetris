d32a39c9b224c5a45494c43a85994785cbac54cd
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-23 23:52:35 +0300

    Fixes bug with rotation Removes manual limit

--- Assets/Scripts/TetrominoController.cs
+++ Assets/Scripts/TetrominoController.cs
@@ -26,7 +26,7 @@ public class TetrominoController : MonoBehaviour
             if (!couldMove)
             {
                 PlaceTetrominoOnPlayArea(ActiveTetromino);
-                ActiveTetromino.transform.position = new Vector3(6.0f, 18.0f, 0.0f);
+                ActiveTetromino.transform.position = new Vector3(6.0f, 18.0f, 0.0f) + ActiveTetromino.PositioningOffset;
             }
         }
 
@@ -34,35 +34,19 @@ public class TetrominoController : MonoBehaviour
         {
             while(TryMoveTetromino(ActiveTetromino, new Vector2(0.0f, -1.0f)));
             PlaceTetrominoOnPlayArea(ActiveTetromino);
-            ActiveTetromino.transform.position = new Vector3(6.0f, 18.0f, 0.0f);
+            ActiveTetromino.transform.position = new Vector3(6.0f, 18.0f, 0.0f) + ActiveTetromino.PositioningOffset;
         }
 
         if (Input.GetKeyDown(KeyCode.Z))
+        {
             TryRotateTetromino(ActiveTetromino, new Vector3(0.0f, 0.0f, -90.0f));
+            AdjustTetriminoChildBlocksRotation(ActiveTetromino);
+        }
             
         if (Input.GetKeyDown(KeyCode.X))
-            TryRotateTetromino(ActiveTetromino, new Vector3(0.0f, 0.0f, 90.0f));
-    }
-
-    private void LimitTetrominoInsidePlayArea(Tetromino tetromino)
-    {
-        if (tetromino == null)
-            return;
-        
-        Rect worldPlayArea = this.GameArea.WorldPlayArea;
-        for(int i=0; i < tetromino.ChildBlocks.Length; ++i)
         {
-            while (tetromino.ChildBlocks[i].transform.position.x < worldPlayArea.xMin)
-                tetromino.transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-
-            while (tetromino.ChildBlocks[i].transform.position.x > worldPlayArea.xMax)
-                tetromino.transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-
-            while (tetromino.ChildBlocks[i].transform.position.y < worldPlayArea.yMin)
-                tetromino.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-
-            while (tetromino.ChildBlocks[i].transform.position.y > worldPlayArea.yMax)
-                tetromino.transform.position += new Vector3(0.0f, -1.0f, 0.0f);
+            TryRotateTetromino(ActiveTetromino, new Vector3(0.0f, 0.0f, 90.0f));
+            AdjustTetriminoChildBlocksRotation(ActiveTetromino);
         }
     }
 
@@ -70,7 +54,6 @@ public class TetrominoController : MonoBehaviour
     {
         Vector3 previousPosition = ActiveTetromino.transform.position;
         ActiveTetromino.transform.position = previousPosition + new Vector3(moveVector.x, moveVector.y, 0.0f);;
-        LimitTetrominoInsidePlayArea(tetromino);
 
         for (int i = 0; i < tetromino.ChildBlocks.Length; i++)
         {
@@ -89,13 +72,10 @@ public class TetrominoController : MonoBehaviour
     private bool TryRotateTetromino(Tetromino tetromino, Vector3 eulerAngles)
     {
         Vector3 previousEulerAngles = ActiveTetromino.transform.rotation.eulerAngles;
-        ActiveTetromino.transform.rotation = Quaternion.Euler(previousEulerAngles + eulerAngles);
-        LimitTetrominoInsidePlayArea(tetromino);
+        Vector3 newEulerAngles = previousEulerAngles + eulerAngles;
+        ActiveTetromino.transform.rotation = Quaternion.Euler(newEulerAngles);
 
-        for (int i = 0; i < tetromino.ChildBlocks.Length; i++)
-            tetromino.ChildBlocks[i].transform.rotation = Quaternion.identity;
-
-        Vector2[] testOffsets = SuperRotationSystem.GetTestOffsetsForDefaultTetromino(previousEulerAngles.z, eulerAngles.z);
+        Vector2[] testOffsets = SuperRotationSystem.GetTestOffsetsForDefaultTetromino(previousEulerAngles.z, newEulerAngles.z);
         for (int i = 0; i < testOffsets.Length; i++)
         {
             if (TryMoveTetromino(tetromino, testOffsets[i]))
@@ -103,9 +83,13 @@ public class TetrominoController : MonoBehaviour
         }
 
         ActiveTetromino.transform.rotation = Quaternion.Euler(previousEulerAngles);
+        return false;
+    }
+
+    private void AdjustTetriminoChildBlocksRotation(Tetromino tetromino)
+    {
         for (int i = 0; i < tetromino.ChildBlocks.Length; i++)
             tetromino.ChildBlocks[i].transform.rotation = Quaternion.identity;
-        return false;
     }
 
     private void PlaceTetrominoOnPlayArea(Tetromino tetromino)
