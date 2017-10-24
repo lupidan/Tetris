534e39f1faf2b546231cc46c4d4c90d3527790ff
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-24 23:30:19 +0300

    Adds rotation type

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    public enum WallKickType
+    {
+        DefaultRotation,
+        ITetrominoRotation
+    }
+
     public Vector3 PositioningOffset { get { return _positioningOffset; } }
+    public WallKickType WallKickTypee { get { return _wallKickType; } }
     public Block[] ChildBlocks { get { return _childBlocks; } }
 
     [SerializeField] private Vector3 _positioningOffset;
+    [SerializeField] private WallKickType _wallKickType;
     [SerializeField] private Block[] _childBlocks;
 }
