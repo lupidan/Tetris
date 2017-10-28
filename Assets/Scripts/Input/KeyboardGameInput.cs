f576a634db046f45adc96a98c64bee8d1375f69f
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-29 00:01:33 +0300

    Adds GameInput interface, and KeyboardGameInput implementation of it

---
+++ Assets/Scripts/Input/KeyboardGameInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class KeyboardGameInput : GameInput
+{
+	public bool MoveLeft { get { return Input.GetKeyDown(KeyCode.LeftArrow); } }
+	public bool MoveRight { get { return Input.GetKeyDown(KeyCode.RightArrow); } }
+	public bool MoveDown { get { return Input.GetKeyDown(KeyCode.DownArrow); } }
+	public bool RotateClockwise { get { return Input.GetKeyDown(KeyCode.Z); } }
+	public bool RotateCounterClockwise { get { return Input.GetKeyDown(KeyCode.X); } }
+	public bool HardDrop { get  { return Input.GetKeyDown(KeyCode.UpArrow); } }
+}
