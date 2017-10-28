2dc3e3079be1b09400ea29064cf4af94874fd634
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-28 23:46:18 +0300

    Adds score controller interface

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,14 @@
+﻿
+public delegate void ScoreControllerEvent<T>(T argument);
+
+public interface ScoreController
+{
+	long Score { get; }
+	long Highscore { get; }
+
+	event ScoreControllerEvent<long> OnScoreUpdate;
+	event ScoreControllerEvent<long> OnHighscoreUpdate;
+
+	void UpdateScore(int[] destroyedLines, Tetromino usedTetromino);
+	void ResetScore();
+}
