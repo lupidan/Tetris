805d91d84e90d251e8d4be20cb1c764f04e65ae0
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-29 12:51:55 +0200

    Uses new constants and tab to spaces

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,138 +4,138 @@ using UnityEngine.UI;
 
 namespace Tetris
 {
-	public class MainMenu : MonoBehaviour
-	{
-		[SerializeField] private Text _gameTitleLabel;
-		[SerializeField] private Text _problemsLabel;
-		[SerializeField] private InputField _widthInputField;
-		[SerializeField] private InputField _heightInputField;
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Text _gameTitleLabel;
+        [SerializeField] private Text _problemsLabel;
+        [SerializeField] private InputField _widthInputField;
+        [SerializeField] private InputField _heightInputField;
 
-		private RectTransform _widthInputFieldRectTransform;
-		private RectTransform _heightInputFieldRectTransform;
+        private RectTransform _widthInputFieldRectTransform;
+        private RectTransform _heightInputFieldRectTransform;
 
-		private Vector2 _gameTitleLabelOriginalAnchorPos;
-		private Vector2 _widthInputFieldOriginalAnchorPos;
-		private Vector2 _heightInputFieldOriginalAnchorPos;
+        private Vector2 _gameTitleLabelOriginalAnchorPos;
+        private Vector2 _widthInputFieldOriginalAnchorPos;
+        private Vector2 _heightInputFieldOriginalAnchorPos;
 
-		private GameController _gameController;
+        private GameController _gameController;
 
-		#region MonoBehaviour
+        #region MonoBehaviour
 
-		private void Awake()
-		{
-			_gameTitleLabelOriginalAnchorPos = _gameTitleLabel.rectTransform.anchoredPosition;
+        private void Awake()
+        {
+            _gameTitleLabelOriginalAnchorPos = _gameTitleLabel.rectTransform.anchoredPosition;
 
-			_widthInputFieldRectTransform = _widthInputField.transform as RectTransform;
-			_widthInputFieldOriginalAnchorPos = _widthInputFieldRectTransform.anchoredPosition;
+            _widthInputFieldRectTransform = _widthInputField.transform as RectTransform;
+            _widthInputFieldOriginalAnchorPos = _widthInputFieldRectTransform.anchoredPosition;
 
-			_heightInputFieldRectTransform = _heightInputField.transform as RectTransform;
-			_heightInputFieldOriginalAnchorPos = _heightInputFieldRectTransform.anchoredPosition;
+            _heightInputFieldRectTransform = _heightInputField.transform as RectTransform;
+            _heightInputFieldOriginalAnchorPos = _heightInputFieldRectTransform.anchoredPosition;
 
-			_problemsLabel.text = "";
-			_widthInputField.text = DefaultWidth.ToString();
-			_heightInputField.text = DefaultHeight.ToString();
-		}
+            _problemsLabel.text = "";
+            _widthInputField.text = PlayfieldConstants.DefaultWidth.ToString();
+            _heightInputField.text = PlayfieldConstants.DefaultHeight.ToString();
+        }
 
-		private void OnEnable()
-		{
-			_gameTitleLabel.rectTransform.DOKill();
-			_gameTitleLabel.rectTransform.anchoredPosition = _gameTitleLabelOriginalAnchorPos;
+        private void OnEnable()
+        {
+            _gameTitleLabel.rectTransform.DOKill();
+            _gameTitleLabel.rectTransform.anchoredPosition = _gameTitleLabelOriginalAnchorPos;
 
-			_gameTitleLabel.rectTransform.DOAnchorPosY(_gameTitleLabelOriginalAnchorPos.y - 30.0f, 1.0f)
-				.SetLoops(-1, LoopType.Yoyo)
-				.SetEase(Ease.InOutQuad);
-		}
+            _gameTitleLabel.rectTransform.DOAnchorPosY(_gameTitleLabelOriginalAnchorPos.y - 30.0f, 1.0f)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutQuad);
+        }
 
-		#endregion
+        #endregion
 
-		#region Public methods
+        #region Public methods
 
-		public void Initialize(GameController gameController)
-		{
-			_gameController = gameController;
-		}
+        public void Initialize(GameController gameController)
+        {
+            _gameController = gameController;
+        }
 
-		#endregion
+        #endregion
 
-		#region Button Actions
+        #region Button Actions
 
-		public void StartButtonWasSelected()
-		{
-			bool shakeWidthInputField = false;
-			bool shakeHeightInputField = false;
-			string gridConfigError = "";
+        public void StartButtonWasSelected()
+        {
+            bool shakeWidthInputField = false;
+            bool shakeHeightInputField = false;
+            string gridConfigError = "";
 
-			int width = DefaultWidth;
-			if (!int.TryParse(_widthInputField.text, out width))
-			{
-				gridConfigError += "Please input a correct Width.\n";
-				shakeWidthInputField = true;
-			}
+            int width = PlayfieldConstants.DefaultWidth;
+            if (!int.TryParse(_widthInputField.text, out width))
+            {
+                gridConfigError += "Please input a correct Width.\n";
+                shakeWidthInputField = true;
+            }
 
-			int height = DefaultHeight;
-			if (!int.TryParse(_heightInputField.text, out height))
-			{
-				gridConfigError += "Please input a correct Height.\n";
-				shakeHeightInputField = true;
-			}
-				
-			if (width > height)
-			{
-				gridConfigError += "Width can't be greater than height.\n";
-				width = height;
-				shakeWidthInputField = true;
-			}
+            int height = PlayfieldConstants.DefaultHeight;
+            if (!int.TryParse(_heightInputField.text, out height))
+            {
+                gridConfigError += "Please input a correct Height.\n";
+                shakeHeightInputField = true;
+            }
+                
+            if (width > height)
+            {
+                gridConfigError += "Width can't be greater than height.\n";
+                width = height;
+                shakeWidthInputField = true;
+            }
 
-			if (width < MinWidth)
-			{
-				gridConfigError += string.Format("Width can't be smaller than {0}.\n", MinWidth);
-				width = MinWidth;
-				shakeWidthInputField = true;
-			}
-			else if (width > MaxWidth)
-			{
-				gridConfigError += string.Format("Width can't be greater than {0}.\n", MaxWidth);
-				width = MaxWidth;
-				shakeWidthInputField = true;
-			}
+            if (width < PlayfieldConstants.MinWidth)
+            {
+                gridConfigError += string.Format("Width can't be smaller than {0}.\n", PlayfieldConstants.MinWidth);
+                width = PlayfieldConstants.MinWidth;
+                shakeWidthInputField = true;
+            }
+            else if (width > PlayfieldConstants.MaxWidth)
+            {
+                gridConfigError += string.Format("Width can't be greater than {0}.\n", PlayfieldConstants.MaxWidth);
+                width = PlayfieldConstants.MaxWidth;
+                shakeWidthInputField = true;
+            }
 
-			if (height < MinHeight)
-			{
-				gridConfigError += string.Format("Height can't be smaller than {0}.\n", MinHeight);
-				width = MinHeight;
-				shakeHeightInputField = true;
-			}
-			else if (height > MaxHeight)
-			{
-				gridConfigError += string.Format("Height can't be greater than {0}.\n", MaxHeight);
-				width = MaxHeight;
-				shakeHeightInputField = true;
-			}		
+            if (height < PlayfieldConstants.MinHeight)
+            {
+                gridConfigError += string.Format("Height can't be smaller than {0}.\n", PlayfieldConstants.MinHeight);
+                width = PlayfieldConstants.MinHeight;
+                shakeHeightInputField = true;
+            }
+            else if (height > PlayfieldConstants.MaxHeight)
+            {
+                gridConfigError += string.Format("Height can't be greater than {0}.\n", PlayfieldConstants.MaxHeight);
+                width = PlayfieldConstants.MaxHeight;
+                shakeHeightInputField = true;
+            }		
 
-			_problemsLabel.text = gridConfigError;
-			if (gridConfigError.Length != 0)
-			{
-				if (shakeWidthInputField)
-				{
-					_widthInputFieldRectTransform.DOKill();
-					_widthInputFieldRectTransform.anchoredPosition = _widthInputFieldOriginalAnchorPos;
-					_widthInputFieldRectTransform.DOShakeAnchorPos(0.2f, 5.0f, vibrato: 100, fadeOut: false);
-				}
-					
-				if (shakeHeightInputField)
-				{
-					_heightInputFieldRectTransform.DOKill();
-					_heightInputFieldRectTransform.anchoredPosition = _heightInputFieldOriginalAnchorPos;
-					_heightInputFieldRectTransform.DOShakeAnchorPos(0.2f, 5.0f, vibrato: 100, fadeOut: false);
-				}
-			}
-			else
-			{
-				_gameController.StartGame(width, height);
-			}
-		}
+            _problemsLabel.text = gridConfigError;
+            if (gridConfigError.Length != 0)
+            {
+                if (shakeWidthInputField)
+                {
+                    _widthInputFieldRectTransform.DOKill();
+                    _widthInputFieldRectTransform.anchoredPosition = _widthInputFieldOriginalAnchorPos;
+                    _widthInputFieldRectTransform.DOShakeAnchorPos(0.2f, 5.0f, vibrato: 100, fadeOut: false);
+                }
+                    
+                if (shakeHeightInputField)
+                {
+                    _heightInputFieldRectTransform.DOKill();
+                    _heightInputFieldRectTransform.anchoredPosition = _heightInputFieldOriginalAnchorPos;
+                    _heightInputFieldRectTransform.DOShakeAnchorPos(0.2f, 5.0f, vibrato: 100, fadeOut: false);
+                }
+            }
+            else
+            {
+                _gameController.StartGame(width, height);
+            }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
