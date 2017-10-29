4c720f40499ea2c4c18239b00e9431d7b8c45120
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-29 14:56:09 +0200

    Adds TetrominoSpawner and default implementation

---
+++ Assets/Scripts/Tetromino/DefaultTetrominoSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Tetris
+{
+    public class DefaultTetrominoSpawner : MonoBehaviour
+    {
+        [Header("Prefabs")]
+        public Tetromino[] TetrominoPrefabs;
+
+        public Tetromino SpawnRandomTetrominoAtPosition(Vector3 position)
+        {
+            Tetromino tetromino = Instantiate(TetrominoPrefabs[UnityEngine.Random.Range(0,7)]);
+            tetromino.transform.position = position + tetromino.PositioningOffset;
+            return tetromino;
+        }
+
+        public void DiscardTetromino(Tetromino tetromino)
+        {
+            Destroy(tetromino.gameObject);
+        }
+    }
+}
