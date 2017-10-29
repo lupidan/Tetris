906991a472fca69488e2fa6a91bd84cc11e4c2e5
Daniel Lupiañez Casares <lupidan@gmail.com>
2017-10-29 15:29:57 +0200

    Adds extension for shuffling

---
+++ Assets/Scripts/Extensions/IListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class IListExtensions
+{
+    private static Random randomNumberGenerator = new Random();  
+
+    public static void Shuffle<T>(this IList<T> list)  
+    {
+        if (list.Count <= 0)
+            return;
+
+        // Fisher–Yates shuffle
+        for (int i = list.Count - 1; i > 0; --i)
+        {
+            int j = randomNumberGenerator.Next(i);
+            T value = list[j];
+            list[j] = list[i];
+            list[i] = value;
+        }
+    }
+}
